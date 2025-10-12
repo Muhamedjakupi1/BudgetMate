495660110fc653d6198ea705662e4650105e2880
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-10-13 00:01:42 +0200

    Update Profile
    Shrimi i permbajtjes ne profile

--- budget-mate/app/screens/Profile.tsx
+++ budget-mate/app/screens/Profile.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 
 const Profile = () => {
+    const user = {
+        name: "Besa Gashi",
+        email: "BesaGashi@example.com",
+        profilePic: require('../../assets/images/favicon.png'),
+        totalBudget: 500,
+        totalSpent: 275,
+        totalDoneExpenses: 5,
+    };
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View><Text>Home Page</Text></View>
+        <ScrollView>
+            <View>
+                <Image source={user.profilePic}></Image>
+                    <Text>{user.name}</Text>
+                    <Text>{user.email}</Text>
+            </View>
+            <View>
+                <View><Text>Budget</Text></View>
+                <View><Text>Spend</Text></View>
+                <View><Text>Done</Text></View>
+            </View>
+
+            <TouchableOpacity><Text>Edit Profile</Text></TouchableOpacity>
+            <TouchableOpacity><Text>Logout</Text></TouchableOpacity>
+
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f7f7'
+    },
+})
