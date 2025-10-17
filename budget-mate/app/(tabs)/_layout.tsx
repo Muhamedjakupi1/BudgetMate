e6ed1ea57ff96c6d7754a47fe68a8b1c9eeda8f5
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-10-17 14:37:57 +0200

    Update layout
    inkuadrimi i budgetContext

--- budget-mate/app/(tabs)/_layout.tsx
+++ budget-mate/app/(tabs)/_layout.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Text } from 'react-native';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Colors } from '@/constants/theme';
+import { BudgetProvider } from '@/constants/budgetContext';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <BudgetProvider>
     <Tabs
       screenOptions={{
         headerShown: false,
@@ -51,5 +53,6 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
+    </BudgetProvider>
   );
 }
