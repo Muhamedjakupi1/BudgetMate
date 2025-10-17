7357f61b99f5ccb527560f983a5e026365de3d41
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-10-17 18:40:19 +0200

    stilized no expense view

--- budget-mate/app/(tabs)/index.tsx
+++ budget-mate/app/(tabs)/index.tsx
@@ -30,65 +30,101 @@ export default function HomePage() {
       <View style={styles.header}>
         <Text style={styles.budgetText}>💵 Budget: ${balance}</Text>
       </View>
+      {pendingExpenses.length > 0 ? (
       <FlatList
         data={pendingExpenses.sort((a, b) => b.id.localeCompare(a.id))}
         keyExtractor={(item) => item.id}
         renderItem={renderExpense}
         contentContainerStyle={styles.listContainer}
       />
+      ) : 
+      (
+        <View style={styles.noExpenseContainer}>
+            <Text style={styles.noExpenseEmoji}>🎉</Text>
+            <Text style={styles.noExpenseTitle}>No Pending Expenses!</Text>
+            <Text style={styles.noExpenseSubtitle}>
+                All your expenses are settled{"\n"}
+                Add some income to get started
+            </Text>
+        </View>
+      )
+      }
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: { 
-    flex: 1, 
-    backgroundColor: "#f7f7f7", 
-    paddingHorizontal: 20, 
-    paddingTop: 20 
+        flex: 1, 
+        backgroundColor: "#f7f7f7", 
+        paddingHorizontal: 20, 
+        paddingTop: 20,
+    },
+    header: { 
+        flexDirection: "row", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        marginBottom: 20 
 },
-  header: { 
-    flexDirection: "row", 
-    justifyContent: "center", 
-    alignItems: "center", 
-    marginBottom: 20 
-},
-  budgetText: { 
-    fontSize: 18, 
-    fontWeight: "bold", 
-    color: "#333" 
-},
-  listContainer: { 
-    paddingBottom: 20 
-},
-  expenseItem: { 
-    backgroundColor: "#fff", 
-    borderRadius: 10, 
-    padding: 15, 
-    marginBottom: 10, 
-    flexDirection: "row", 
-    justifyContent: "space-between", 
-    alignItems: "center", 
-    shadowColor: "#000", 
-    shadowOffset: { width: 1, height: 7 }, 
-    shadowOpacity: 0.25, 
-    shadowRadius: 5, 
-    elevation: 5 
-  },
-  expenseTitle: { 
-    fontSize: 16, 
-    fontWeight: "600", 
-    color: "#333" 
-},
-  expenseAmount: { 
-    fontSize: 14, 
-    color: "#777" 
-},
-  buttonsContainer: { 
-    flexDirection: "row", 
-    gap: 10 },
-  btn: { 
-    paddingHorizontal: 15, 
-    paddingVertical: 8, 
-    borderRadius: 5 },
+    budgetText: { 
+        fontSize: 18, 
+        fontWeight: "bold", 
+        color: "#333" 
+    },
+    listContainer: { 
+        paddingBottom: 20 
+    },
+    expenseItem: { 
+        backgroundColor: "#fff", 
+        borderRadius: 10, 
+        padding: 15, 
+        marginBottom: 10, 
+        flexDirection: "row", 
+        justifyContent: "space-between", 
+        alignItems: "center", 
+        shadowColor: "#000", 
+        shadowOffset: { width: 1, height: 7 }, 
+        shadowOpacity: 0.25, 
+        shadowRadius: 5, 
+        elevation: 5 
+    },
+    expenseTitle: { 
+        fontSize: 16, 
+        fontWeight: "600", 
+        color: "#333" 
+    },
+    expenseAmount: { 
+        fontSize: 14, 
+        color: "#777" 
+    },
+    buttonsContainer: { 
+        flexDirection: "row", 
+        gap: 10 },
+    btn: { 
+        paddingHorizontal: 15, 
+        paddingVertical: 8, 
+        borderRadius: 5 
+    },
+    noExpenseContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    noExpenseEmoji: {
+        fontSize: 50,
+        marginBottom: 15,
+    },
+    noExpenseTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#2E8B57',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    noExpenseSubtitle: {
+        fontSize: 16,
+        color: '#666',
+        textAlign: 'center',
+        lineHeight: 22,
+    },
 });
