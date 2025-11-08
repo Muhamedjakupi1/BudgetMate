18377c8b48aced00e592f447d938a29c4c429730
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-11-08 17:04:32 +0100

    set up firebase project

---
+++ budget-mate/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGq-YDuolUWFI_FFPBvwvRvwnPLjeFGII",
+  authDomain: "budgetmate-1b9b3.firebaseapp.com",
+  projectId: "budgetmate-1b9b3",
+  storageBucket: "budgetmate-1b9b3.firebasestorage.app",
+  messagingSenderId: "1990732227",
+  appId: "1:1990732227:web:08b5842efecf1d6731f576",
+  measurementId: "G-DRG1Z04DN6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export default app;
